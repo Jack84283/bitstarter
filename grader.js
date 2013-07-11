def900616ee05526337b19f5dc7e87bcb7e9bc4c
Ubuntu <ubuntu@ip-10-164-26-125.ec2.internal>
2013-07-11 06:39:14 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -15,7 +15,7 @@ References:
    - https://github.com/visionmedia/commander.js
    - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
- + JSON
+ + JSONvi
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -55,6 +57,16 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkString = function(str, checksfile) {
+	var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = str.indexOf(checks[ii]) > -1;
+        out[checks[ii]] = present;
+    }
+    return out;
+}
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +77,22 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <html_url>', 'URL to index.html', null, null)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	
+	var checkJson;
+	if (program.url == null) {
+		checkJson = checkHtmlFile(program.file, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	} else {
+		rest.get(program.url, {}).on('complete', function(data) {
+			checkJson = checkString(data, program.checks);
+			var outJson = JSON.stringify(checkJson, null, 4);
+			console.log(outJson);
+		});
+	}
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
